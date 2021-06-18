2793fc0a22e9c68e9a5c5846a792dae02cbb2df6
Emanuel Toledo <ematolr@gmail.com>
2021-06-17 19:24:15 -0500

    Finished Lodash

--- _.js
+++ _.js
@@ -81,7 +81,16 @@ const _ ={
     let droppedArray = this.drop(array, dropNumber);
     return droppedArray;
   },
-  
+
+  chunk(array, size=1){
+    let arrayChunks = [];
+    for (let i = 0; i < array.length; i += size) {
+      let arrayChunk = array.slice(i, i+size);
+      arrayChunks.push(arrayChunk);
+    }
+    return arrayChunks
+    
+  }
 
 };
 
